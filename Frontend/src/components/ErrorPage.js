b72fc956b984bc232e5252cb18f1da7fb15fb97f
Shubham Rawalkar <shubhamrawalkar826@gmail.com>
2022-10-13 18:14:43 +0530

    5th commit;

--- Frontend/src/components/ErrorPage.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class ErrorPage extends Component {
-    render() {
-        return (
-            <div><h4 className='text-red'>invalid pathname</h4></div>
-        )
-    }
-}
